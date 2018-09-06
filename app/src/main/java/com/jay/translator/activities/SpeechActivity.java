6cd9b7cd0ef1e558f1e5ac27df0d0712176179cb
JaySokirko <sokirko0601@gmail.com>
2018-09-06 19:41:31 +0300

    jay

--- app/src/main/java/com/jay/translator/activities/SpeechActivity.java
+++ app/src/main/java/com/jay/translator/activities/SpeechActivity.java
@@ -2,38 +2,42 @@ package com.jay.translator.activities;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
-import android.widget.ListView;
-import android.widget.NumberPicker;
 import android.widget.TextView;
 
 import com.jay.translator.R;
 import com.jay.translator.adapters.RecyclerViewAdapter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Locale;
 
 public class SpeechActivity extends AppCompatActivity {
 
     private static final String TAG = "TAG";
-//    private TextView speechText;
+    //    private TextView speechText;
     private AnimationDrawable backgroundAnimation;
 
     private ArrayList<Integer> images = new ArrayList<>();
     private ArrayList<String> names = new ArrayList<>();
 
     private static final int REQ_CODE_SPEECH_INPUT = 100;
+    private RecyclerViewAdapter adapterInputLanguages;
+    private RecyclerViewAdapter adapterOutputLanguages;
+    private RecyclerView recyclerViewInputLang;
+    private LinearLayoutManager managerInputLang;
+
+    private TextView languageFromHint;
+    private TextView languageToHint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,51 +45,35 @@ public class SpeechActivity extends AppCompatActivity {
         setContentView(R.layout.activity_speech);
 
         LinearLayout bottomSheet = findViewById(R.id.bottom_sheet);
+        languageFromHint = findViewById(R.id.language_from_recycler);
+        languageToHint = findViewById(R.id.language_to_recycler);
 
         backgroundAnimation = (AnimationDrawable) bottomSheet.getBackground();
         backgroundAnimation.setExitFadeDuration(4000);
 
-        images.add(R.drawable.london);
-        images.add(R.drawable.moscow);
-        images.add(R.drawable.berlin);
-        images.add(R.drawable.paris);
-        images.add(R.drawable.madrid);
-        images.add(R.drawable.venice);
+        initializeRecyclerView();
 
-        names.add("English");
-        names.add("Russian");
-        names.add("Deutsch");
-        names.add("Français");
-        names.add("Español");
-        names.add("Italiano");
+        adapterInputLanguages.setOnItemClickListener(new RecyclerViewAdapter.ClickListener() {
+            @Override
+            public void onItemClick(int position, View v) {
 
-        LinearLayoutManager manager = new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false);
-        final RecyclerView recyclerView = findViewById(R.id.languages_from_recycler);
-        recyclerView.setLayoutManager(manager);
-        RecyclerViewAdapter adapter = new RecyclerViewAdapter(images,names,this);
-        recyclerView.setAdapter(adapter);
-
-         adapter.setOnItemClickListener(new RecyclerViewAdapter.ClickListener() {
-             @Override
-             public void onItemClick(int position, View v) {
-
-                 Log.d(TAG, "onItemClick: " + position);
-
-             }
-
-             @Override
-             public void onItemLongClick(int position, View v) {
-
-             }
-         });
+                switch (position){
+                    case 0 :
+                        languageFromHint.setText("English");
+                        break;
+                    case 1:
+                        languageFromHint.setText("Russian");
+                }
+            }
+        });
     }
 
 
-    public void onSpeechToText(View view){
+    public void onSpeechToText(View view) {
 
-        String language =  "es";
+        String language = "es";
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,language);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, language);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, language);
         intent.putExtra(RecognizerIntent.EXTRA_ONLY_RETURN_LANGUAGE_PREFERENCE, language);
@@ -128,5 +116,37 @@ public class SpeechActivity extends AppCompatActivity {
 
         if (backgroundAnimation != null && backgroundAnimation.isRunning())
             backgroundAnimation.stop();
+    }
+
+
+    private void initializeRecyclerView() {
+        images.add(R.drawable.london);
+        images.add(R.drawable.moscow);
+        images.add(R.drawable.berlin);
+        images.add(R.drawable.paris);
+        images.add(R.drawable.madrid);
+        images.add(R.drawable.venice);
+
+        names.add("English");
+        names.add("Russian");
+        names.add("Deutsch");
+        names.add("Français");
+        names.add("Español");
+        names.add("Italiano");
+
+        managerInputLang = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        LinearLayoutManager managerOutputLang = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+
+        recyclerViewInputLang = findViewById(R.id.languages_from_recycler);
+        recyclerViewInputLang.setLayoutManager(managerInputLang);
+
+        RecyclerView recyclerViewOutputLang = findViewById(R.id.languages_to_recycler);
+        recyclerViewOutputLang.setLayoutManager(managerOutputLang);
+
+        adapterInputLanguages = new RecyclerViewAdapter(images, names, this);
+        adapterOutputLanguages = new RecyclerViewAdapter(images, names, this);
+
+        recyclerViewInputLang.setAdapter(adapterInputLanguages);
+        recyclerViewOutputLang.setAdapter(adapterOutputLanguages);
     }
 }
--- app/src/main/java/com/jay/translator/adapters/RecyclerViewAdapter.java
+++ app/src/main/java/com/jay/translator/adapters/RecyclerViewAdapter.java
@@ -1,13 +1,19 @@
 package com.jay.translator.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -21,6 +27,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     private ArrayList<Integer> images;
     private ArrayList<String> names;
     private Context context;
+
+    private static CheckBox lastChecked = null;
+    private static int lastCheckedPos = 0;
 
     private static ClickListener clickListener;
 
@@ -48,6 +57,9 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         holder.name.setText(names.get(position));
     }
 
+
+
+
     @Override
     public int getItemCount() {
         return names.size();
@@ -57,12 +69,14 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         private ImageView image;
         private TextView name;
+        private CheckBox checkBox;
 
-        public ViewHolder(View itemView) {
+        ViewHolder(View itemView) {
             super(itemView);
 
             image = itemView.findViewById(R.id.icon_language);
             name = itemView.findViewById(R.id.hint_language);
+            checkBox = itemView.findViewById(R.id.check_box);
 
             itemView.setOnClickListener(this);
 
@@ -76,7 +90,6 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     public interface ClickListener {
         void onItemClick(int position, View v);
-        void onItemLongClick(int position, View v);
     }
 
     public void setOnItemClickListener(ClickListener clickListener) {
